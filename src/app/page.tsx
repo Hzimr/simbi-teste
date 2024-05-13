d51da113789e7f003fb2ba3344739068b77bd58b
HzimR <hitalo005@gmail.com>
2024-05-13 17:01:51 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-slate-900 flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-slate-100 text-4xl">Hello Simbi</h1>
+    </main>
+  );
+}
