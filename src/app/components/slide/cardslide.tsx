52466354e52c2652130cbadfcca5a33b5b63a65c
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-14 18:50:33 -0300

    feat: updated and finalized cardslide

--- src/app/components/slide/cardslide.tsx
+++ src/app/components/slide/cardslide.tsx
@@ -1,35 +1,45 @@
+import { FaRegHeart } from 'react-icons/fa6'
+import { formatPrice } from '../../utils/formatPrice'
 
 export function CardSlide() {
   return (
-    <div className="border-radius max-w-96 border-[1px] border-solid border-gray-300 p-5">
-      <header>
-        <div className="">ROUANET</div>
-        <h2>Projeto Teste</h2>
+    <div className="max-w-96 rounded-md border-[1px] border-solid border-gray-200 p-5 shadow-lg">
+      <header className="flex flex-col gap-2 ">
+        <span className="bg-rouanet text-textrouanet flex w-3/12 items-center justify-center p-[1px] text-sm font-semibold">
+          ROUANET
+        </span>
+        <h2>
+          <strong>Projeto Teste</strong>
+        </h2>
         <h3 className="text-zinc-500">São Paulo • SP</h3>
       </header>
-      <p className="text-justify">
+      <p className="mt-3 text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae enim
         modi vel odio, sed quos eaque quo voluptas ratione delectus, magnam
         unde. Sequi odit explicabo dolorem velit praesentium voluptatem
         provident.
       </p>
-      <section>
+      <section className="mt-3 flex flex-col gap-3">
         <span className="flex gap-2 ">
-          <p className="w-28">Aprovado</p>
+          <p className="w-2/5">Aprovado</p>
           <p>
-            <strong>R$ 100.000,00</strong>
+            <strong>{formatPrice(10000000)}</strong>
           </p>
         </span>
         <span className="flex gap-2 ">
-          <p className="w-28">Captado</p>
+          <p className="w-2/5">Captado</p>
           <p>
-            <strong>R$ 100.000,00</strong>
+            <strong>{formatPrice(10000000)}</strong>
           </p>
         </span>
       </section>
-      <div className="flex items-center gap-4">
-        <button className="flex-1 bg-zinc-100 p-3">Adicionar</button>
-        <button>Coração</button>
+      <div className="mt-3 flex items-center gap-4">
+        <button className="flex-1 bg-zinc-100 p-3 text-zinc-400">
+          ADICIONAR
+        </button>
+        <button className="transition ease-in-out hover:opacity-70">
+          <FaRegHeart size={32} color="#BDBDBD" />
+        </button>
       </div>
     </div>
   )
