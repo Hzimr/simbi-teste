7cbef910d2ece52447ab21835b2d41a6f3f05ccf
HzimR <hitalo005@gmail.com>
2024-05-15 22:39:15 -0300

    refactor: deleted CardSlide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { CardSlide } from './components/cardslide/cardslide'
 import { Slide } from './components/slide/slide'
 
 export default function Home() {
@@ -7,8 +6,7 @@ export default function Home() {
       <h1 className="text-xl font-semibold text-link">
         Ver outros Projetos do Proponente
       </h1>
-      <CardSlide />
-      <div className="h-48 w-[90vw] ">
+      <div className="h-[70vh] w-[90vw] ">
         <Slide />
       </div>
     </main>
