131942de9e00ff09e628beb088914f5fe1508e4a
HzimR <hitalo005@gmail.com>
2024-05-15 22:40:47 -0300

    necessary refactor: use cardSlide in Slide

--- src/app/components/slide/slide.tsx
+++ src/app/components/slide/slide.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
 
@@ -17,8 +17,32 @@ import {
   HashNavigation,
   Keyboard,
 } from 'swiper/modules'
+import { formatPrice } from '@/app/utils/formatPrice'
+import { FaRegHeart } from 'react-icons/fa6'
+
+interface CardProps {
+  id_projeto: number
+  nome: string
+  municipio: string
+  uf: string
+  valor_aprovado: number
+  valor_captado: number
+}
 
 export function Slide() {
+  const [card, setCard] = useState<CardProps[]>([])
+
+  async function loadCardData() {
+    const response = await fetch('http://localhost:3333/dados')
+    const data = await response.json()
+
+    setCard(data.card)
+  }
+
+  useEffect(() => {
+    loadCardData()
+  }, [])
+
   return (
     <>
       <Swiper
@@ -32,6 +56,7 @@ export function Slide() {
         }}
         pagination={{
           clickable: true,
+          dynamicBullets: true,
         }}
         breakpoints={{
           480: {
@@ -50,11 +75,59 @@ export function Slide() {
         modules={[Pagination, Navigation, HashNavigation, Keyboard]}
         className="mySwiper"
       >
-        <SwiperSlide data-hash="slide1">Slide 1</SwiperSlide>
-        <SwiperSlide data-hash="slide2">Slide 2</SwiperSlide>
-        <SwiperSlide data-hash="slide3">Slide 3</SwiperSlide>
-        <SwiperSlide data-hash="slide4">Slide 4</SwiperSlide>
-        <SwiperSlide data-hash="slide5">Slide 5</SwiperSlide>
+        <div>
+          {card.map((value) => {
+            return (
+              <SwiperSlide data-hash={value.id_projeto} key={value.id_projeto}>
+                <article>
+                  <div className="max-w-96 rounded-md border-[1px] border-solid border-gray-200 p-5 shadow-lg">
+                    <header className="flex h-32 flex-col items-start gap-8 ">
+                      <span className="flex w-3/12 items-center justify-center bg-rouanet p-[1px] text-sm font-semibold text-textrouanet">
+                        ROUANET
+                      </span>
+                      <section>
+                        <h2 className="text-justify">
+                          <strong>{value.nome}</strong>
+                        </h2>
+                        <h3 className="text-justify text-zinc-500">
+                          {value.municipio} • {value.uf}
+                        </h3>
+                      </section>
+                    </header>
+                    <p className="mt-8 text-justify">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Beatae enim modi vel odio, sed quos eaque quo voluptas
+                      ratione delectus, magnam unde. Sequi odit explicabo
+                      dolorem velit praesentium voluptatem provident.
+                    </p>
+                    <section className="mt-3 flex flex-col items-start gap-3">
+                      <span className="flex gap-2 ">
+                        <p className="w-2/5">Aprovado</p>
+                        <p>
+                          <strong>{formatPrice(value.valor_aprovado)}</strong>
+                        </p>
+                      </span>
+                      <span className="flex gap-2">
+                        <p className="w-2/5">Captado</p>
+                        <p>
+                          <strong>{formatPrice(value.valor_captado)}</strong>
+                        </p>
+                      </span>
+                    </section>
+                    <div className="mt-3 flex items-center gap-4">
+                      <button className="flex-1 bg-zinc-100 p-3 text-zinc-400">
+                        ADICIONAR
+                      </button>
+                      <button className="transition ease-in-out hover:opacity-70">
+                        <FaRegHeart size={32} color="#BDBDBD" />
+                      </button>
+                    </div>
+                  </div>
+                </article>
+              </SwiperSlide>
+            )
+          })}
+        </div>
       </Swiper>
     </>
   )
